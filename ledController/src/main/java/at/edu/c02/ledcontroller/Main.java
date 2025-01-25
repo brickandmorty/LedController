dd0b82f74a2e4fe2978bb596ff39c7d75bd2bedf
Leo <leonid.tahiraj@tutanota.com>
2025-01-25 12:56:57 +0100

    Main für groupstatus erweitert

--- ledController/src/main/java/at/edu/c02/ledcontroller/Main.java
+++ ledController/src/main/java/at/edu/c02/ledcontroller/Main.java
@@ -1,11 +1,15 @@
 package at.edu.c02.ledcontroller;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        String groupName = "";
         LedController ledController = new LedControllerImpl(new ApiServiceImpl());
 
         String input = "";
@@ -19,15 +23,43 @@ public class Main {
 
             if (input.equalsIgnoreCase("demo")) {
                 ledController.demo();
-            } else if (input.startsWith("group")) {
+            }
+            else if(input.startsWith("groupstatus")) {
+                if (groupName.equals("")){
+                    System.out.println("Please insert group name first!");
+                } else {
+                    System.out.println("Fetching LED Status for group: " + groupName);
+                    JSONArray groupLeds = ledController.getGroupLeds(groupName);
+
+                    for (int i = 0; i < groupLeds.length(); i++) {
+                        // Get the current LED object (JSONObject)
+                        JSONObject led = groupLeds.getJSONObject(i);
+
+                        // Get the "on" status for this LED
+                        boolean isOn = led.getBoolean("on");
+
+                        if (isOn) {
+                            System.out.println("LED " + led.getInt("id") + " is currently on: Color: " + led.getString("color"));
+                        } else {
+                            System.out.println("LED " + led.getInt("id") + " is currently off: " + isOn + " Color: " + led.getString("color"));
+
+                        }
+
+                    }
+                }
+            }
+            else if (input.startsWith("group")) {
                 try {
-                    String groupName = input.split(" ", 2)[1]; // Gruppenname extrahieren
+                    groupName = input.split(" ", 2)[1]; // Gruppenname extrahieren
                     System.out.println("Fetching LEDs for group: " + groupName);
                     System.out.println(ledController.getGroupLeds(groupName).toString(2));
                 } catch (Exception e) {
                     System.err.println("Invalid input. Please use 'group <name>'.");
                 }
             }
+
+
         }
     }
+}
 
