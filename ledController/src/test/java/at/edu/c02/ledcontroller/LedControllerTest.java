7c8b10139c67abae1535941c1e6f20d94d463933
Leo <leonid.tahiraj@tutanota.com>
2025-01-25 12:19:46 +0100

    Mock Test für groups closes #7

--- ledController/src/test/java/at/edu/c02/ledcontroller/LedControllerTest.java
+++ ledController/src/test/java/at/edu/c02/ledcontroller/LedControllerTest.java
@@ -1,8 +1,16 @@
 package at.edu.c02.ledcontroller;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 public class LedControllerTest {
@@ -14,4 +22,26 @@ public class LedControllerTest {
     public void dummyTest() {
         assertEquals(1, 1);
     }
+
+    @Test
+    public void mockGroupsTest() throws Exception{
+        ApiServiceImpl apiService = mock(ApiServiceImpl.class);
+
+        String jsonResponse = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("api.json").toURI())));
+
+        when(apiService.getLights()).thenReturn(new JSONObject(jsonResponse));
+
+        LedControllerImpl controller = new LedControllerImpl(apiService);
+
+        JSONArray result = controller.getGroupLeds("A");
+
+        assertNotNull(result);
+
+        // Read the expected result from the group_A_result.json file
+        String expectedJsonResponse = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("group_A_result.json").toURI())));
+        JSONObject expectedResult = new JSONObject(expectedJsonResponse);
+
+        assertEquals(expectedResult.getJSONArray("lights").toString(), result.toString());
+    }
+
 }
