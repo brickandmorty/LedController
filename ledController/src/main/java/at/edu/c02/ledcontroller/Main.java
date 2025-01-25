52cfa3e3c298dfeb3bd01a4d45fa70ac56ad4dfe
Leo <leonid.tahiraj@tutanota.com>
2025-01-25 13:00:59 +0100

    Main für groupstatus erweitert

--- ledController/src/main/java/at/edu/c02/ledcontroller/Main.java
+++ ledController/src/main/java/at/edu/c02/ledcontroller/Main.java
@@ -43,6 +43,7 @@ public class Main {
                         } else {
                             System.out.println("LED " + led.getInt("id") + " is currently off: " + isOn + " Color: " + led.getString("color"));
 
+
                         }
 
                     }
